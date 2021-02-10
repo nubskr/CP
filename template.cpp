134f47f57f240f1ae345afea119e30592198f365
nubskr <dakshrinwan2003@gmail.com>
2021-02-10 13:39:30 +0530

    Template for Programming Competitions

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define endl '\n'
+#define IO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define      sortA(v)          sort(v.begin(), v.end())
+#define      sortD(v)          sort(v.begin(), v.end(), greater<auto>())
+
+const int N = 1e9;
+
+void solve(){
+	
+}
+
+int main()
+{
+	IO;
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int t = 1;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+    return 0;
+} 
+
